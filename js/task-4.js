d7b75b11987de8a15562ba9e6f7f8ec16ed12036
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-03-20 20:00:12 -0400

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,13 +1,13 @@
 
 const loginForm = document.querySelector(".login-form");
-loginForm.addEventListener(".submit", (event) => {
+loginForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const { email, password } = event.target.elements;
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
 
-    if (emailValue === "" || password === "") {
+    if (emailValue === "" || passwordValue === "") {
         alert("All form fields must be filled in");
         return;
     }
